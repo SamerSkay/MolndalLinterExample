69f43153d3d8878391b4a8cc23f9d3c2b5890657
SamerSkay <142941807+SamerSkay@users.noreply.github.com>
2024-09-09 13:16:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const x = 5;
-const y = 7;
+const x = 5
+const y = 7
 
-function double(x) { 
-    return x * 2; 
+function double(x) {
+    return x * 2
 }
 
-console.log("Dubbelt av 5 är " + double(x));
+console.log("Dubbelt av 5 är " + double(x))
+console.log("Dubbelt av 7 är " + double(y))
